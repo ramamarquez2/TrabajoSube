acca6c634a7536c97c5bd1856d4a55c7647a2215
mauricio gonzales <mgonzales@ips.edu.ar>
2023-08-28 12:07:42 -0300

    creo una funcion de ejemplo

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -1,5 +1,14 @@
 <?php
 namespace TrabajoSube;
 class Colectivo{
+    protected $linea;
     
+    public function __construct($linea){
+        $this->linea = $linea;
+    }
+    
+    //    Funcion de ejemplo para test
+    public function getLinea(){
+        return $this->linea;
+    }
 }
--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -5,5 +5,9 @@ namespace TrabajoSube;
 use PHPUnit\Framework\TestCase;
 
 class ColectivoTest extends TestCase{
-   
+
+    public function testGetlinea(){
+        $cole = new Colectivo(103);
+        $this->assertEquals($cole->getLinea(), 103);
+    }
 }
