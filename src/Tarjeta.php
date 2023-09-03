57861f2a2c51b07825353d32be25152be4085173
ramamarquez2 <ramamarquez2@gmail.com>
2023-09-02 22:07:50 -0300

    add deuda logic & convert int vars to float

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -2,24 +2,44 @@
 namespace TrabajoSube;
 class Tarjeta{
     public string $propietario;
-    public int $saldo;
+    public float $saldo;
+    public float $deuda;
     public array $ifSaldo = [150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 2500, 3000, 3500, 4000];
     public array $boletos = []; 
 
     public function __construct($name, $s=0){
         $this->propietario = $name;
-        if(!($s > 6600)){
+        if(!($s > 6600 || $s < (-211.84))){
             $this->saldo = $s;
         }
         else {
             $this->saldo = 0;
         }
+        if($s>=0){
+            $this->deuda = 0;
+        }
+        else{
+            $this->deuda = $this->saldo * (-1);
+        }
     }
 
     public function cargarSaldo($numSaldo){
         if(in_array($numSaldo, $this->ifSaldo) && ($numSaldo + $this->saldo) <= 6600){
-            $this->saldo += $numSaldo;
             echo "Usted le cargo " . $numSaldo . ".\n";
+            if($numSaldo + $this->saldo >= 0){
+                $this->saldo = $this->saldo + $numSaldo - $this->deuda;
+                echo "Se le desconto " . $this->deuda . ".\n";
+                if($this->saldo >= 0){
+                    $this->deuda = 0;
+                }
+                else{
+                    $this->deuda = $this->saldo * (-1);
+                }
+            }
+            else{
+                $this->saldo += $numSaldo;
+                $this->deuda = $this->saldo * (-1);
+            }
         }
         else{
             echo "El numero es incorrecto.";
