f4b72bfa92b7cf4d774726702efe1914dcde569d
ramamarquez2 <ramamarquez2@gmail.com>
2023-09-13 22:33:13 -0300

    Add Franquicias logic

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -2,14 +2,27 @@
 namespace TrabajoSube;
 class Colectivo{
     protected int $linea; //protected
+    protected float $descuento;
 
     public function __construct($linea){
         $this->linea = $linea;
+        $this->$descuento = 1;
     }
 
     public function pagarCon($tarjeta){
-        if(($tarjeta->saldo - 120) >= (-211.84)){
-            $tarjeta->saldo -= 120;
+
+        if(get_class($tarjeta) == "FranquiciaCompleta" || get_class($tarjeta) == "MedioBoleto" )
+        {
+            if($tarjeta->$beneficioRestantes > 0)
+            {
+                $tarjeta->$beneficioRestantes -= 1;
+                $this->descuento = $tarjeta->$descuentoFraccional;
+            }
+        }
+
+        if(($tarjeta->saldo - (120 * $this->descuento)) >= (-211.84)){
+            $tarjeta->saldo -= (120 * $this->descuento);
+            $this->descuento = 1;
             if($tarjeta->saldo < 0){
                 $tarjeta->deuda = $tarjeta->saldo * (-1);
             }
